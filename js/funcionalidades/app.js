5ca2b920f2032df89ceeb1b22cde6fd0933d9331
EudesNunes <araujonuneseudes@gmail.com>
2022-11-06 22:35:48 -0300

    Novos Backgrounds

---
+++ js/funcionalidades/app.js
@@ -0,0 +1,10 @@
+$('nav a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
