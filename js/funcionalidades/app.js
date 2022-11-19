661b01dd8ba5248a2071ef9d32ca390592ddabc1
EudesNunes <araujonuneseudes@gmail.com>
2022-11-19 15:36:10 -0300

    Ajuste cogumelo e velocidade

--- js/funcionalidades/app.js
+++ js/funcionalidades/app.js
@@ -1,10 +1,9 @@
 $('nav a').click(function(e){
 	e.preventDefault();
 	var id = $(this).attr('href'),
-			menuHeight = $('nav').innerHeight(),
 			targetOffset = $(id).offset().top;
 	$('html, body').animate({
-		scrollTop: targetOffset - menuHeight
-	}, 500);
+		scrollTop: targetOffset - 45
+	}, 150);
 });
 
